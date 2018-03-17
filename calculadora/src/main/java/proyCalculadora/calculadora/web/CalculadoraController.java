a14b5478cdb26c3f68952e12a11279b08d696f1d
anton <anton@192.168.1.32>
2018-03-16 23:56:03 -0500

    Completo

--- calculadora/src/main/java/proyCalculadora/calculadora/web/CalculadoraController.java
+++ calculadora/src/main/java/proyCalculadora/calculadora/web/CalculadoraController.java
@@ -11,14 +11,55 @@ import proyCalculadora.calculadora.model.Calculadora;
 @Controller
 public class CalculadoraController {
 
-	@GetMapping("/new")
+	@GetMapping("/calculadora")
 	public String initCreationForm(Model model) {
 		model.addAttribute("calculadora", new Calculadora());
 		return"calcFrm";
 	}
 	
-	@PostMapping("/new")
+	@PostMapping("/resultado")
 	public String submitForm(@ModelAttribute Calculadora calculadora) {
+		
+		double num1 = calculadora.getNum1();
+		double num2 = calculadora.getNum2();
+		String msj = "";
+		
+		switch (calculadora.getOperacion()) {
+		case '+':
+			msj = "La suma de " +Math.round(num1)  + " y " +Math.round(num2)  + " es : " + (num1 + num2);
+			break;
+		case '-':
+			msj = "La resta de " +Math.round(num1)  + " y " + Math.round(num2) + " es : " + (num1 - num2);
+			break;
+		case '*':
+			msj = "La multiplicación de " +Math.round(num1)  + " y " + Math.round(num2) + " es : " + (num1 * num2);
+			break;
+		case '^':
+			msj = "La raiz de " +Math.round(num1)  + " radicando " + Math.round(num2) + " es : " + (Math.pow(num1, 1/num2));
+			break;
+		case '/':
+			
+			//NO SE PUEDE DIVIDIR UN NUMERO ENTRE CERO
+			if (num2 != 0) {
+				
+				msj = "La división de " + num1 + " y " + num2 + " es : " + (num1 / num2);
+				
+			}else {
+				
+				msj = "El segundo número debe ser diferente de cero";
+				
+			}
+			
+			break;
+		default:
+			
+			msj = "Las operaciones válidas son suma (+), resta (-), multiplicación (*), divisón (/)";
+			
+			break;
+		}
+		
+		calculadora.setMensaje(msj);
+		
 		return "resultFrm";
 		
 	}
